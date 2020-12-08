d41675dd02fcd430fe3001e338e3e3cc2c5dd3a3
Matthew Congrove <mattcongrove@gmail.com>
2020-12-07 22:35:06 -0600

    Finish list component styles

--- components/ListRow/index.tsx
+++ components/ListRow/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import {
+	Image,
 	StyleProp,
 	StyleSheet,
 	Text,
@@ -8,34 +9,137 @@ import {
 } from 'react-native';
 import { color } from '../../styles';
 
-interface Props {
+interface CommitAuthor {
+	login: string;
+	avatar_url: string;
+}
+
+interface CommitCommitter {
+	date: string;
+}
+
+interface CommitVerification {
+	verified: boolean;
+}
+
+interface CommitDetail {
 	message: string;
+	verification: CommitVerification;
+	committer: CommitCommitter;
+}
+
+interface Commit {
+	sha: string;
+	commit: CommitDetail;
+	author: CommitAuthor;
+}
+
+interface Props {
+	commit: Commit;
 	style?: StyleProp<TextStyle>;
 }
 
 const ListRow: FC<Props> = ({
-	message = "",
+	commit = null,
 	style = null,
 }) => {
-	return (
+	const ts = new Date(commit?.commit.committer.date || '');
+
+	return commit && (
 		<View
 			style={[style, styles.Wrapper]}
 		>
 			<Text
 				style={styles.Message}
 			>
-				{message}
+				{commit.commit.message}
 			</Text>
+			<View
+				style={styles.Metadata}
+			>
+				<View
+					style={styles.Author}
+				>
+					<Image
+						style={styles.Avatar}
+						source={{
+							uri: commit.author.avatar_url
+						}}
+					/>
+					<Text
+						style={styles.Name}
+					>
+						{commit.author.login}
+					</Text>
+				</View>
+
+				<Text
+					style={styles.SHA}
+				>
+					{commit.sha.substring(0, 7)}
+				</Text>
+
+				<Text
+					style={styles.Time}
+				>
+					{ts.toLocaleString([], { 
+						hour: 'numeric', 
+						minute: '2-digit'
+					})}
+				</Text>
+			</View>
 		</View>
 	);
 };
 
 export const styles = StyleSheet.create({
 	Wrapper: {
-		backgroundColor: color.Grey800,
+		marginBottom: 12,
+		padding: 18,
+		borderRadius: 6,
+		backgroundColor: color.Grey700,
 	},
 	Message: {
+		marginBottom: 12,
+		fontSize: 18,
+		lineHeight: 24,
+		fontWeight: '600',
 		color: color.White,
+	},
+	Metadata: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	Author: {
+		flexGrow: 1,
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	Avatar: {
+		width: 24,
+		height: 24,
+		borderRadius: 12,
+		marginRight: 6,
+	},
+	Name: {
+		marginRight: 12,
+		fontSize: 14,
+		lineHeight: 14,
+		fontWeight: '400',
+		color: color.Grey500,
+	},
+	SHA: {
+		marginRight: 12,
+		fontSize: 14,
+		lineHeight: 14,
+		fontWeight: '400',
+		color: color.Grey500,
+	},
+	Time: {
+		fontSize: 14,
+		lineHeight: 14,
+		fontWeight: '400',
+		color: color.Grey500,
 	},
 });
 
